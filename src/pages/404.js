1a508f1edfb91f1a1e67ddda4cfad6b99797c89c
Rodolpho Caetano <rphfti@hotmail.com>
2020-03-18 19:39:22 -0300

    Adicionando o import Link

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,7 @@ import SEO from "../components/seo";
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404</h1>
-    
+    <h1>404</h1>    
   </Layout>
 )
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from "gatsby";
 
 const AboutPage = () => (
-  <h1>About page</h1>
+  <>
+    <h1>About page</h1>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+    </ul>
+  </>
 )
 
 export default AboutPage;
