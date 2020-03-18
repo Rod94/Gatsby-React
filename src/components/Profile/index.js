85387e35b1805ce012929d6f3f2161fba41e2eb6
Rodolpho Caetano <rphfti@hotmail.com>
2020-03-18 19:46:32 -0300

    Adicionando a parte de Profile em components

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => (
+  <div className="profile-wrapper">
+    <h1>Rodolpho Caetano</h1>
+    <h2>Programador FrontEnd</h2>
+    <p>Fazendo curso de Gatsby + React do Willian Justen</p>
+  </div>
+)
+
+export default Profile;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from "gatsby";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 const AboutPage = () => (
-  <>
+  <Layout>
+    <SEO title="About" />
     <h1>About page</h1>
     <ul>
       <li>
         <Link to="/">Home</Link>
       </li>
     </ul>
-  </>
+  </Layout>
 )
 
 export default AboutPage;
