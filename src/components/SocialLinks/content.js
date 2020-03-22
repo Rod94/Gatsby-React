4ad2c818318156829f4a6b06ecfc361a2fe35072
Rodolpho Caetano <rphfti@hotmail.com>
2020-03-22 19:57:09 -0300

    Criando os social links

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,16 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/Rod94'
+  },
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/RodolphoCaetano'
+  },
+  {
+    label: 'Instagram',
+    url: 'https://www.instagram.com/rodolphocaetano/'
+  }
+]
+
+export default links;
---
+++ src/components/SocialLinks/icons.js
@@ -0,0 +1,11 @@
+import { Github } from '@styled-icons/fa-brands/GithubSquare';
+import { Twitter } from '@styled-icons/fa-brands/TwitterSquare';
+import { Instagram } from '@styled-icons/fa-brands/InstagramSquare';
+
+const Icons = {
+  Github,
+  Twitter,
+  Instagram
+}
+
+export default Icons;
