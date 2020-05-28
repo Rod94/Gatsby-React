ea31bbbfb224a45f5f33838fe3e15ac9454e6026
Rodolpho Caetano <rphfti@hotmail.com>
2020-05-28 18:54:01 -0300

    change about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { Link } from "gatsby";
+
 import Layout from "../components/Layout";
 import SEO from "../components/seo";
 
 const AboutPage = () => (
   <Layout>
     <SEO title="About" />
-    <h1>About page</h1>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-    </ul>
   </Layout>
 )
 
