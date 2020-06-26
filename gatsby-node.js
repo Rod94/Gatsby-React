17f5a62e77815d75636dc3a50f1266d5fdb452e0
Rodolpho Caetano <rphfti@hotmail.com>
2020-06-26 15:28:56 -0300

    Criado template blog-post

--- gatsby-node.js
+++ gatsby-node.js
@@ -23,26 +23,28 @@ exports.onCreateNode = ({ node, getNode, actions}) => {
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   return graphql(`
-   {
-    allMarkdownRemark {
-      edges {
-        node {
-          fields {
-            slug
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
           }
         }
       }
     }
-  }
-`).then(result => {
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPage({
-      path: node.fields.slug,
-      component: path.resolve("./src/template/blog-post.js"),
-      context: {
-        slug: node.fields.slug
-      },
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          slug: node.fields.slug,
+        },
+      })
     })
   })
-})
 }
